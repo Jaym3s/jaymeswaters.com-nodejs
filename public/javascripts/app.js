8ceff6a48f3e251539d1ac4cef20dae39f564ee1
Jaymes Waters <dev+jaym3s@bendyworks.com>
2011-08-04 19:08:13 -0500

    first attempt

---
+++ public/javascripts/app.js
@@ -0,0 +1,11 @@
+$(function() {
+  var tree = $('#tree');
+  $.FractalTree({ color: "#c00"
+                , initX: 200
+                , initY: 200
+                , branch_length: 58
+                , max_size: 10
+                , max_sub_branch: 6
+                , canvas: tree.get(0)
+  });
+});
