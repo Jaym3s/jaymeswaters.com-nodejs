d04b22d0b7b785a357b72a9b9d9bf1d3c78cf508
Jaymes Waters <Jaym3s@gmail.com>
2011-08-15 14:00:24 -0500

    WIP tweaking branch direction

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -3,9 +3,9 @@ $(function() {
   $.FractalTree({ color: "#c00"
                 , initX: 200
                 , initY: 200
-                , branch_length: 58
-                , max_size: 10
-                , max_sub_branch: 6
+                // , branch_length: 58
+                // , max_size: 10
+                // , max_sub_branch: 6
                 , canvas: tree.get(0)
   });
 });
--- public/javascripts/tree.js
+++ public/javascripts/tree.js
@@ -2,48 +2,59 @@
   var undefined;
 
   $.FractalTree = function(opts) {
-    var ctx,
+    var context,
     cfg = $.extend(true, {
       initX: 400,
       initY: 400,
-      max_sub_branch: 5,
-      max_sub_angle: 3*Math.PI/4,
-      max_size: 5,
-      branch_length: 75,
+      max_sub_branch: 24,
+      max_sub_angle: (3 * Math.PI/5)/2, //3*Math.PI/4,
+      max_size: 12,
+      branch_length: 64,
       color: "#fff"
     }, opts);
 
+    R: 50
+    G: 95
+    B: 35
     function init() {
       if(cfg.canvas !== undefined) {
-        ctx = cfg.canvas.getContext("2d");
-        ctx.strokeStyle = cfg.color;
-        _makeBranch(cfg.initX, cfg.initY, cfg.branch_length, -Math.PI/2, cfg.max_size);
+        context = cfg.canvas.getContext("2d");
+        _makeBranch({start_x: cfg.initX
+                  , start_y: cfg.initY
+                  , length: cfg.branch_length
+                  , angle: Math.abs(-Math.PI/2/Math.PI) - 90
+                  , size: cfg.max_size
+        });
         cfg
       }
     }
 
-    function _makeBranch(start_x, start_y, length, angle, size) {
-      if (size > 0) {
-        ctx.lineWidth = size;
-        ctx.beginPath();
-        ctx.moveTo(start_x, start_y);
+    function _makeBranch(opts) {
+      if (opts.size > 0) {
+        context.lineWidth = opts.size;
+        context.strokeStyle = cfg.color;
+        context.beginPath();
+        context.moveTo(opts.start_x, opts.start_y);
 
-        var end_x = start_x + length * Math.cos(angle);
-        var end_y = start_y + length * Math.sin(angle);
-        ctx.lineTo(end_x, end_y);
-        ctx.stroke();
+        var end_x = opts.start_x + opts.length * Math.cos(opts.angle);
+        var end_y = opts.start_y + opts.length * Math.sin(opts.angle);
+        context.lineTo(end_x, end_y);
+        context.stroke();
 
-        var sub_branch = Math.random(cfg.max_sub_branch - 1) + 2;
+        var sub_branch = Math.random(cfg.max_sub_branch);
         var branch_length_dimin = .5 + Math.random()/2;
 
         for(var i=0; i < sub_branch; i++) {
-          var newLength = length * branch_length_dimin;
-          var newAngle = angle + Math.random() * cfg.max_sub_angle - cfg.max_sub_angle / 2;
-          var newSize = size - 1;
-          _makeBranch (end_x, end_y, newLength, newAngle, newSize);
-
+          var newLength = opts.length * branch_length_dimin;
+          var newAngle = opts.angle + Math.random() * cfg.max_sub_angle - cfg.max_sub_angle / 2;
+          var newSize = opts.size - 1;
+          _makeBranch({start_x: end_x
+                    , start_y: end_y
+                    , length: newLength
+                    , angle: newAngle
+                    , size: newSize});
         }
-        ctx.closePath();
+        context.closePath();
       }
     }
 
