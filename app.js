2d8c3aec3b2424f41dd57fcecc08830587d5c178
Jaymes Waters <Jaym3s@gmail.com>
2011-09-17 10:53:50 -0500

    add socket io

--- app.js
+++ app.js
@@ -3,7 +3,8 @@
  * Module dependencies.
  */
 
-var express = require('express');
+var express = require('express')
+    io = require('socket.io');
 var app = module.exports = express.createServer();
 
 // Configuration
@@ -50,3 +51,12 @@ app.post('/update_ip', function(req, res) {
 
 app.listen(3000);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+
+// sockets
+
+var sio = io.listen(app);
+
+sio.sockets.on('connection', function (socket) {
+    console.log('A socket connected!');
+});
+
