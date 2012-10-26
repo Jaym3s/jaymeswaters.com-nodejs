4dab6a8cd65f4aefa8998df8596923f00ad8f2b9
Jaymes Waters <Jaym3s@gmail.com>
2012-10-26 10:55:04 -0500

    Add makisu js dropdown

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -2,8 +2,37 @@ var _gaq = _gaq || [];
 _gaq.push(['_setAccount', 'UA-30733671-1']);
 _gaq.push(['_trackPageview']);
 
-(function() {
+$().ready(function() {
   var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
   ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
   var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+
+  if ( $.fn.makisu.enabled ) {
+
+    var $maki = $( '.list' );
+
+    console.log(' // Create Makisus');
+    $maki.makisu({ selector: 'li', overlap: 0.6, speed: 0.85 });
+
+    // Open all
+
+    $( '.list' ).makisu( 'open' );
+
+    // Toggle on click
+
+    // $( '.toggle' ).on( 'click', function() {
+      // $( '.list' ).makisu( 'toggle' );
+    // });
+
+    // Disable all links
+
+    // $( '.demo a' ).click( function( event ) {
+      // event.preventDefault();
+    // });
+
+  } else {
+
+    $( '.warning' ).show();
+  }
+
 })();
