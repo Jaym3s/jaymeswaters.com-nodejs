5cec696a3501b3901b8b271d8e3139123678bb62
Jaymes Waters <Jaym3s@gmail.com>
2016-01-13 10:32:22 -0600

    Update url

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ config = {
     // When running Ghost in the wild, use the production environment.
     // Configure your URL and mail settings here
     production: {
-        url: 'https://jaymes.us',
+        url: 'https://waters.today',
         mail: {},
         database: {
             client: 'mysql',
